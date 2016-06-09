798435457bf3e0522033da4614624329653752fb
brodd <jimmy.brodd@gmail.com>
2016-06-09 10:52:39 +0200

    Make default size of crop tool the same as the shortest side of the image

--- lib/src/main/java/com/soundcloud/android/crop/CropImageActivity.java
+++ lib/src/main/java/com/soundcloud/android/crop/CropImageActivity.java
@@ -229,8 +229,8 @@ public class CropImageActivity extends MonitoredActivity {
 
             Rect imageRect = new Rect(0, 0, width, height);
 
-            // Make the default size about 4/5 of the width or height
-            int cropWidth = Math.min(width, height) * 4 / 5;
+            // Make the default size same as the shortest side
+            int cropWidth = Math.min(width, height);
             @SuppressWarnings("SuspiciousNameCombination")
             int cropHeight = cropWidth;
 
